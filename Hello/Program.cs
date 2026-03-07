98e59b9c0d43bfd487b32c48efd221f65783760f
Sunny Gupta <guptakamlesh469@gmail.com>
2026-03-07 15:02:39 +0530

    Finished with List and Dictionary and started with Functions.

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; //For Dictionary and List
 using System.Linq;
-using System.Runtime.InteropServices;
+using System.Runtime.InteropServices; //For "Optional" keyword
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
@@ -384,11 +384,67 @@ namespace Hello
             //}
 
             //Lists
+            //List<int> listNumber = new List<int>();
+            //for (int i = 0; i < 3; i++) {
+            //    Console.WriteLine("Enter a number: ");
+            //    listNumber.Add(Convert.ToInt32(Console.ReadLine()));
+            //}
+            //for (int i = 0; i < listNumber.Count; i++) {
+            //    Console.WriteLine(listNumber[i]);
+            //}
+            //listNumber.RemoveAt(1);
+            //foreach (int num in listNumber) {
+            //    Console.WriteLine(num);
+            //}
 
+            //Dictionary
+            //Dictionary<int, string> names = new Dictionary<int, string> {
+            //    //Key pairs
+            //    {1, "Sunny"},
+            //    {2, "Saurabh"},
+            //    {3, "Sahil"}
+            //};
+            //for (int i = 0; i < names.Count; i++) { 
+            //    KeyValuePair<int, string> pair = names.ElementAt(i);
+            //    Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+            //}
+            //foreach (KeyValuePair<int, string> item in names)
+            //{
+            //    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            //}
+            //Dictionary<string, string> teacher = new Dictionary<string, string>
+            //{
+            //    {"Maths", "Pushpa"},
+            //    {"SPM", "Divya"}
+            //};
+            ////Console.WriteLine(teacher["maths"]); //Does'nt work
+            //if (teacher.TryGetValue("Maths", out string teachers))
+            //{
+            //    Console.WriteLine(teachers);
+            //    teacher["Math"] = "Joe";
+            //}
+            //else { 
+            //    Console.WriteLine("Math teacher not found");
+            //}
+            //if (teacher.ContainsKey("SPM"))
+            //{
+            //    teacher.Remove("SPM");
+            //}
+            //else {
+            //    Console.WriteLine("SPM teacher not found");
+            //}
+            //foreach (KeyValuePair<string, string> item in teacher)
+            //{
+            //    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            //}
 
             //Methods
             //Greet("Sunny");
             //Console.WriteLine(Average(4, 5));
+
+
+            //Optional parameters
+            Console.WriteLine(Add(5));
 
             //OOPS
             //Player player1 = new Player();
@@ -396,6 +452,10 @@ namespace Hello
             //player1.setHealth(50);
 
             Console.ReadLine();
+        }
+
+        static int Add(int a, [Optional] int b) {
+            return a + b;
         }
     }
 }
