6d6dab1e239e179e56c7e2406c34258fa36bb0a2
Sunny Gupta <guptakamlesh469@gmail.com>
2026-03-07 12:48:22 +0530

    Completed with Array and functions of it. [Arrays, Array sorting, Array reversal, Array clearing, Array IndexOf].

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -314,7 +314,76 @@ namespace Hello
             //{
             //    Console.Write($"{num} ");
             //}
-            const int angleCount = 3;
+            //const int angleCount = 3;
+            //int[] angles = new int[angleCount];
+            //for (int i = 0; i < angleCount; i++) {
+            //    Console.WriteLine($"Enter angle{i+1}: ");
+            //    angles[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //int angleSum = 0;
+            //for (int i = 0; i < angleCount; i++) {
+            //    angleSum += angles[i];
+            //}
+            //Console.WriteLine(angleSum);
+            //Console.WriteLine(angleSum == 100 ? "Valid" : "Invalid");
+
+            //Array Sorting
+            //int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+            //Array.Sort(numbers);
+            //foreach (var num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+            //Array Reversal
+            //int[] numbers = new int[] { 0, 1, 2, 3, 4, 5 };
+            /*Array.Reverse(numbers);
+            foreach (var num in numbers)
+            {
+                Console.Write($"{num} ");
+            }*/
+            //int[] sortedNumbers = new int[numbers.Length];    //Alternative to Array.Reverse() method
+            //int x = 0;
+            //for (int i = numbers.Length - 1; i >= 0; i--)
+            //{
+            //    sortedNumbers[x] = numbers[i];
+            //    x++;
+            //}
+            //foreach (var num in sortedNumbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            //Array clearing
+            //int[] numbers = new int[] { 0, 1, 2, 3, 4,5,6,7,8,9};
+            //Array.Clear(numbers, 0, numbers.Length);
+            //foreach (var num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+            //for (int i = 0; i < numbers.Length; i++) {
+            //    numbers[i] = default;
+            //}
+            //Array.Clear(numbers, 5, 5);
+            //foreach (var num in numbers)
+            //{
+            //    Console.Write($"{num} ");
+            //}
+
+            //Array IndexOf
+            //int[] numbers = new int[] { 90, 199, 50, 30 };
+            //Console.Write("Enter number to search: ");
+            //int search = Convert.ToInt32(Console.ReadLine());
+            //int position = Array.IndexOf(numbers, search, 1, 3);      //1 is the starting index and 3 is the count of elements to search
+            //if (position > -1)
+            //{
+            //    Console.WriteLine($"Number {search} has been found at position {position + 1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Number {search} has not been found");
+            //}
+
+            //Lists
 
 
             //Methods
